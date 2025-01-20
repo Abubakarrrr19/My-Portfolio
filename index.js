7eeb4b9615cb05fb3d21ee5ac4901c24cad6b6f1
Abubakarrrr19 <abubakarrrr857@gmail.com>
2025-01-20 22:32:54 +0500

    added project files

---
+++ index.js
@@ -0,0 +1,12 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar-2');
+    sidebar.style.display = "flex";
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar-2');
+    sidebar.style.display = "none";
+}
+
+
+  
